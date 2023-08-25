b2d8d1ffb0dfeae454d176eb4e887aa0b1963c8b
lulamae12 <tls.tommy11@gmail.com>
2023-08-25 11:32:47 -0400

    commit

--- buildEDUMatches.py
+++ buildEDUMatches.py
@@ -1,6 +1,8 @@
 #https://github.com/nickdenardis/edu-inventory/blob/master/edu-list.txt
 #quick and dirty way to build possible canvas urls
 
+import requests,time
+
 inFile = open("in.txt","r")
 
 inLines = inFile.readlines()
@@ -10,9 +12,16 @@ print(len(inLines))
 outfile = open("out.txt","a")
 
 for line in inLines:
-    print(f"https://canvas.{line.strip()}/*")
 
-    formattedString = '"https://canvas.'+line.strip()+'/*",\n'
+    #print(line)
 
-    outfile.write(formattedString)
+    try:
+        url = requests.get(line)
+        print(line)
+        print(url.status_code)
+        formattedString = '"https://canvas.'+line.strip()+'/*",\n'
+
+        outfile.write(formattedString)
+    except:
+        continue
 outfile.close()
